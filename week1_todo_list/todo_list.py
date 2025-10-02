4e8a299321e48ea08ecfb5aaf23fb89a961c8ec9
Lee Sze Ning <lsning98@gmail.com>
2025-10-02 15:48:35 +0800

    Initial clean commit

---
+++ week1_todo_list/todo_list.py
@@ -0,0 +1,17 @@
+sample_list_data = {
+    "title": "My Tasks",
+    "tasks_list": [
+        {
+            "content": "Task A",
+            "checked": True,
+        },
+        {
+            "content": "Task B",
+            "checked": False,
+        },
+        {
+            "content": "Task C",
+            "checked": False,
+        },
+    ],
+}
